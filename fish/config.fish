8421d911573bf9ce3542ad20b3b89cb681e0fbff
joshieingold <joshlynch2001@gmail.com>
2026-08-25 09:08:32 -0300

    test

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,11 @@
 set -g fish_greeting ""
 set -gx NVIM_LISTEN_ADDRESS "/tmp/nvimsocket_$fish_pid"
+
+if not set -q SSH_AUTH_SOCK
+    eval (ssh-agent -c) > /dev/null
+    ssh-add ~/.ssh/id_ed25519 > /dev/null 2>&1
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     fastfetch
